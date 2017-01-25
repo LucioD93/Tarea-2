50395d29714cc44f9c7c7d95f71d0209cb7f964c
LucioD93 <luciodavid93@gmail.com>
2017-01-24 20:25:03 -0400

    Rama Lucio

--- src/main.py
+++ src/main.py
@@ -6,17 +6,50 @@ Created on 24 ene. 2017
 
 from datetime import datetime
 
+import unittest
+from __builtin__ import str
+from _ast import Str
+
+class Prueba(unittest.TestCase):
+    def test_calcularPrecio(self):
+        tarifa = 1.0
+        t1 = datetime.strptime("1 Jan 2017 13:00", "%d %b %Y %H:%M")
+        t2 = datetime.strptime("1 Jan 2017 14:00", "%d %b %Y %H:%M")
+        t = [t1, t2]
+        self.assertEqual(calcularPrecio(tarifa, t), 1.0)
+        tarifa = 0
+        self.assertEqual(calcularPrecio(tarifa, t), None)
+        
+
 def calcularPrecio(tarifa, tiempoDeServicio):
-    print("Tarifa: ", tarifa)
-    print(tiempoDeServicio[0])
-    print(tiempoDeServicio[1])
+    """Funcion para calcular el precio a pagar segun una tarifa y un tiempo de servicio, retorna none si hay un error"""
+    if (not(type(tarifa) is float and tarifa > 0)):
+        return None
+    if (not(type(tiempoDeServicio[0]) is datetime and type(tiempoDeServicio[1]) is datetime)):
+        return None
     deltaTiempo = tiempoDeServicio[1] - tiempoDeServicio[0]
-    print(deltaTiempo.total_seconds()/3600)
-    
-if __name__ == '__main__':
-    tarifa = 1
-    inicio = datetime.now()
-    assert type(inicio) is datetime, "inicio no es una fecha"
-    fin = datetime.strptime("24 Jan 2017 06:30", "%d %b %Y %H:%M")
-    tiempo = [inicio, fin]
-    calcularPrecio(tarifa, tiempo)
+    minutos = deltaTiempo.total_seconds()/60
+    if (not minutos > 15):
+        return None
+    if (not minutos/60 < 7*24):
+        return None
+    return tarifa * (minutos/60)
+
+# if __name__ == '__main__':
+#     tarifa = 1
+#     inicio = datetime.strptime("1 Jan 2017 13:00", "%d %b %Y %H:%M")
+#     fin = datetime.strptime("1 Jan 2017 14:00", "%d %b %Y %H:%M")
+#     tiempo = [inicio, fin]
+#     total = calcularPrecio(tarifa, tiempo)
+#     print("Total: " + repr(total))
+
+unittest.main()
+
+
+
+
+
+
+
+
+
