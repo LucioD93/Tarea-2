7cebdf4fb3cc4af6b34ab7467fcad4e2ead263b4
LucioD93 <luciodavid93@gmail.com>
2017-01-24 20:54:55 -0400

    Version pulida de la funcion

--- src/main.py
+++ src/main.py
@@ -19,7 +19,19 @@ class Prueba(unittest.TestCase):
         self.assertEqual(calcularPrecio(tarifa, t), 1.0)
         tarifa = 0
         self.assertEqual(calcularPrecio(tarifa, t), None)
-        
+        tarifa = 10.0
+        t1 = datetime.strptime("1 Jan 2017 13:00", "%d %b %Y %H:%M")
+        t2 = datetime.strptime("8 Jan 2017 14:00", "%d %b %Y %H:%M")
+        t = [t1, t2]
+        self.assertEqual(calcularPrecio(tarifa, t), None)
+        t1 = datetime.strptime("1 Jan 2017 13:00", "%d %b %Y %H:%M")
+        t2 = datetime.strptime("1 Jan 2017 13:15", "%d %b %Y %H:%M")
+        t = [t1, t2]
+        self.assertEqual(calcularPrecio(tarifa, t), 10.0)
+        t1 = datetime.strptime("1 Jan 2017 13:00", "%d %b %Y %H:%M")
+        t2 = datetime.strptime("2 Jan 2017 13:15", "%d %b %Y %H:%M")
+        t = [t1, t2]
+        self.assertEqual(calcularPrecio(tarifa, t), tarifa*25)
 
 def calcularPrecio(tarifa, tiempoDeServicio):
     """Funcion para calcular el precio a pagar segun una tarifa y un tiempo de servicio, retorna none si hay un error"""
@@ -29,16 +41,20 @@ def calcularPrecio(tarifa, tiempoDeServicio):
         return None
     deltaTiempo = tiempoDeServicio[1] - tiempoDeServicio[0]
     minutos = deltaTiempo.total_seconds()/60
-    if (not minutos > 15):
+    if (not minutos >= 15):
         return None
-    if (not minutos/60 < 7*24):
+    horas = minutos//60
+    minutos = minutos - horas*60
+    if(minutos > 0):
+        horas = horas + 1
+    if (not horas < 7*24):
         return None
-    return tarifa * (minutos/60)
+    return tarifa * horas
 
 # if __name__ == '__main__':
-#     tarifa = 1
+#     tarifa = 10.0
 #     inicio = datetime.strptime("1 Jan 2017 13:00", "%d %b %Y %H:%M")
-#     fin = datetime.strptime("1 Jan 2017 14:00", "%d %b %Y %H:%M")
+#     fin = datetime.strptime("1 Jan 2017 14:15", "%d %b %Y %H:%M")
 #     tiempo = [inicio, fin]
 #     total = calcularPrecio(tarifa, tiempo)
 #     print("Total: " + repr(total))
